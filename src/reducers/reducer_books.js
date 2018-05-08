5e95b7b9171244a82301272210c590df3d316180
RyanKJ <ryankj42@gmail.com>
2018-05-07 19:41:08 -0500

    Initial file upload

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'The 5 Ways', pages: 20},
+    {title: 'Stephen King Stuff', pages: 4000},
+    {title: 'Happy Days', pages: 4},
+    {title: 'Another Book', pages: 534}
+  ]
+}
